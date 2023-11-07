4235e09d183fd2da6a4487b59a730eac4c1f9838
lauralardies <lrodrlar@myuax.com>
2023-11-07 16:10:34 +0100

    comenzamos con builder

--- Patrones Creacionales/Ejercicio 2/builder.py
+++ Patrones Creacionales/Ejercicio 2/builder.py
@@ -4,74 +4,131 @@ from typing import Any
 
 
 class Builder(ABC):
-    """
-    The Builder interface specifies methods for creating the different parts of
-    the Product objects.
-    """
 
     @property
     @abstractmethod
-    def product(self) -> None:
+    def pizza(self) -> None:
         pass
 
     @abstractmethod
-    def produce_part_a(self) -> None:
+    def masa(self) -> None:
         pass
 
     @abstractmethod
-    def produce_part_b(self) -> None:
+    def salsa(self) -> None:
         pass
 
     @abstractmethod
-    def produce_part_c(self) -> None:
+    def ingredientes(self) -> None:
         pass
+
+    @abstractmethod
+    def coccion(self) -> None:
+        pass
+
+    @abstractmethod
+    def presentacion(self) -> None:
+        pass
+
+    @abstractmethod
+    def maridajes(self) -> None:
+        pass
+
+    @abstractmethod
+    def extras(self) -> None:
+        pass
+
 
 
 class ConcreteBuilder1(Builder):
-    """
-    The Concrete Builder classes follow the Builder interface and provide
-    specific implementations of the building steps. Your program may have
-    several variations of Builders, implemented differently.
-    """
 
     def __init__(self) -> None:
-        """
-        A fresh builder instance should contain a blank product object, which is
-        used in further assembly.
-        """
         self.reset()
 
     def reset(self) -> None:
-        self._product = Product1()
+        self._pizza = Pizza()
 
     @property
-    def product(self) -> Product1:
-        """
-        Concrete Builders are supposed to provide their own methods for
-        retrieving results. That's because various types of builders may create
-        entirely different products that don't follow the same interface.
-        Therefore, such methods cannot be declared in the base Builder interface
-        (at least in a statically typed programming language).
-
-        Usually, after returning the end result to the client, a builder
-        instance is expected to be ready to start producing another product.
-        That's why it's a usual practice to call the reset method at the end of
-        the `getProduct` method body. However, this behavior is not mandatory,
-        and you can make your builders wait for an explicit reset call from the
-        client code before disposing of the previous result.
-        """
-        product = self._product
+    def pizza(self) -> Pizza:
+        pizza = self._pizza
         self.reset()
-        return product
+        return pizza
 
-    def produce_part_a(self) -> None:
-        self._product.add("PartA1")
+    def masa(self) -> None:
+        while True:
+            print("Seleccione el tipo de masa: \n- Masa tradicional\n- Masa integral\n- Masa sin gluten\n")
+            opcion = input(">> ")
+            if opcion in ["Masa tradicional", "Masa integral", "Masa sin gluten"]:
+                self._pizza.add(opcion)
+                break
+            else:
+                print("Opción no valida")
 
-    def produce_part_b(self) -> None:
-        self._product.add("PartB1")
+    def salsa(self) -> None:
+        while True:
+            print("Seleccione el tipo de salsa: \n- Salsa de tomate\n- Salsa barbacoa\n- Salsa carbonara\n")
+            opcion = input(">> ")
+            if opcion in ["Salsa de tomate", "Salsa barbacoa", "Salsa carbonara"]:
+                self._pizza.add(opcion)
+                break
+            else:
+                print("Opción no valida")
 
-    def produce_part_c(self) -> None:
-        self._product.add("PartC1")
+    def ingredientes(self) -> None:
+        while True:
+            print("Seleccione los ingredientes separados por comas: \n- Jamón\n- Queso\n- Bacon\n- Cebolla\n- Pimiento\n- Piña\n- Carne picada\n- Pollo\n- Atún\n- Tomate\n- Aceitunas\n- Maíz\n- Champiñones\n- Anchoas\n- Salami\n- Pimiento picante\n- Rúcula\n- Salsa barbacoa\n- Salsa carbonara\n")
+            opciones = input(">> ").split(",")
+            validas = []
+            for opcion in opciones:
+                if opcion not in ["Jamón", "Queso", "Bacon", "Cebolla", "Pimiento", "Piña", "Carne picada", "Pollo", "Atún", "Tomate", "Aceitunas", "Maíz", "Champiñones", "Anchoas", "Salami", "Pimiento picante", "Rúcula", "Salsa barbacoa", "Salsa carbonara"]:
+                    validas.append(opcion)
+                else:
+                    pass
+            if len(validas) == 0:
+                self._pizza.add(opciones)
+                break
+            else:
+                print("Hay alguna opción no válida, vuelva a intentarlo")
+
+    def coccion(self) -> None:
+        while True:
+            print("Seleccione el tipo de cocción: \n- Horno de leña\n- Horno eléctrico\n- Horno de gas\n")
+            opcion = input(">> ")
+            if opcion in ["Horno de leña", "Horno eléctrico", "Horno de gas"]:
+                self._pizza.add(opcion)
+                break
+            else:
+                print("Opción no valida")
+
+    def presentacion(self) -> None:
+        while True:
+            print("Seleccione el tipo de presentación: \n- Pizza entera\n- Pizza por raciones\n")
+            opcion = input(">> ")
+            if opcion in ["Pizza entera", "Pizza por raciones"]:
+                self._pizza.add(opcion)
+                break
+            else:
+                print("Opción no valida")
+
+    def maridajes(self) -> None:
+        #TODO
+        pass
+
+    def extras(self) -> None:
+        while True:
+            print("Seleccione los extras separados por comas: \n- Queso extra\n- Jamón extra\n- Bacon extra\n- Cebolla extra\n- Pimiento extra\n- Piña extra\n- Carne picada extra\n- Pollo extra\n- Atún extra\n- Tomate extra\n- Aceitunas extra\n- Maíz extra\n- Champiñones extra\n- Anchoas extra\n- Salami extra\n- Pimiento picante extra\n- Rúcula extra\n- Salsa barbacoa extra\n- Salsa carbonara extra\n")
+            opciones = input(">> ").split(",")
+            validas = []
+            for opcion in opciones:
+                if opcion not in ["Queso extra", "Jamón extra", "Bacon extra", "Cebolla extra", "Pimiento extra", "Piña extra", "Carne picada extra", "Pollo extra", "Atún extra", "Tomate extra", "Aceitunas extra", "Maíz extra", "Champiñones extra", "Anchoas extra", "Salami extra", "Pimiento picante extra", "Rúcula extra", "Salsa barbacoa extra", "Salsa carbonara extra"]:
+                    validas.append(opcion)
+                else:
+                    pass
+            if len(validas) == 0:
+                self._pizza.add(opciones)
+                break
+            else:
+                print("Hay alguna opción no válida, vuelva a intentarlo")
 
 
 class Product1():
