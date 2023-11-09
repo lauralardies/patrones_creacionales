a70321b9d778263cdc5c5852ff49112f337652a7
lauralardies <lrodrlar@myuax.com>
2023-11-09 10:37:08 +0100

    + coments

--- Patrones Creacionales/Ejercicio 2/run.py
+++ Patrones Creacionales/Ejercicio 2/run.py
@@ -1,32 +1,8 @@
-from director import Director
-from pizza_builder import PizzaBuilder
-from config import limpiar_pantalla
+from main import main
+
 
 if __name__ == "__main__":
-
-    director = Director()
-    builder = PizzaBuilder()
-    director.builder = builder
-
-    while True:
-        limpiar_pantalla()
-        # Primero pedimos crear la pizza
-        print("Crea tu pizza: ")
-        director.construir_completo()
-        builder.pizza.guardar_csv()
-
-        # Luego mostramos la pizza creada
-        limpiar_pantalla()
-        print("Tu pizza: ")
-        builder.pizza.visualizacion_csv()
-
-        # Finalmente preguntamos si le gusta la pizza
-        print("¿Te gusta tu pizza? [Y]/N")
-        respuesta = input('>> ')
-        if respuesta.capitalize() == 'N': # Si no le gusta, volvemos a empezar
-            builder.pizza.borrar_pizza()
-            print('Empecemos de nuevo')
-            input('Presiona cualquier tecla para continuar...')
-        else: # Si le gusta, guardamos la pizza y terminamos
-            print('¡Gracias por tu compra!')
-            break
+    '''
+    Lanzador del programa
+    '''
+    main()
